5372611c6d9a0d4c0bc0a7cdce21471f5193503c
Ranjan Singh <137ranjan@gmail.com>
2023-07-24 19:03:18 +0530

    Versioning Rest API Accept headers versioning (Media Type)

--- src/main/java/com/ranjan/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
+++ src/main/java/com/ranjan/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
@@ -30,4 +30,13 @@ public class VersioningPersonController {
     public PersonV2 getSecondVersionOfPersonHeader() {
         return new PersonV2(new Name("Bob", "Charlie"));
     }
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v1+json")
+    public PersonV1 getFirstVersionOfPersonAcceptHeader() {
+        return new PersonV1("Bob Charlie");
+    }
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v2+json")
+    public PersonV2 getSecondVersionOfPersonAcceptHeader() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
 }
